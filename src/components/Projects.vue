c0f8443ce3b809fd6f0d51e6506f876292b4ec68
Yuki <yutoki0304@gmail.com>
2025-06-09 16:16:45 +0900

    スキルの欄を横並びにしてスッキリさせました

--- src/components/Projects.vue
+++ src/components/Projects.vue
@@ -3,13 +3,8 @@
     <h2 class="project-title">Skills</h2>
     <div class="project-language">
       <ul>
-        <li>HTML/CSS</li>
-        <li>Javascript</li>
-        <li>TypeScript</li>
-        <li>React.js</li>
-        <li>Vue.js</li>
-        <li>gas.js</li>
-        <li>Next.js</li>
+        <li>HTML/CSS,Javascript,TypeScript</li>
+        <li>React.js,Vue.js,gas.js,Next.js</li>
       </ul>
     </div>
   </div>
